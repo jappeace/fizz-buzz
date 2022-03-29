91fd5c6ce54877a9a96a1a76dfdd30395035e53d
Jappie Klooster <jappieklooster@hotmail.com>
2022-03-29 12:00:28 -0400

    This seems to function reasonably
    I feel a little disgusting now.
    I suppose we got akka haskell.
    could've use channels,
    but I deep down want to be exceptional,
    so I chose exceptions.

--- src/Template.hs
+++ src/Template.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Template
   ( main
   )
@@ -9,36 +11,7 @@ import Control.Monad
 import Control.Exception
 import Control.Concurrent
 import Data.IORef
-
-main :: IO ()
-main = do
-  emitter <- forkIO $ emitter
-  fizzTid <- forkIO $ fizz emitter
-  buzzTid <- forkIO $ buzz emitter
-  numsTid  <- forkIO $ numSender emitter
-  let threads = [fizzTid, buzzTid, numsTid, emitter]
-  forever $ do
-    void $ traverse (flip throwTo Flush) threads
-    threadDelay 0_0001_000 -- is this neccisary?
-
-emitter :: IO ()
-emitter = do
-  ref <- newIORef []
-  forever $
-      handle (\case
-                   Flush -> do
-                     res <- readIORef ref
-                     writeIORef ref []
-                     if (hasn't (traversed . failing _Fizz _Buzz) res) then
-                       print $ head $ res ^.. traversed . _Num
-                     else do -- has a fizz or buzz (or both
-                        traverseOf (traversed . _Fizz) (putStr . show)
-                        traverseOf (traversed . _Buzz) (putStr . show)
-                        putStr "\n"
-            ) $ handle (putItIn ref) $ waitMsg
-
-putItIn :: IORef [FizzBuzzMsg] -> FizzBuzzMsg -> IO ()
-putItIn ref msg = modifyIORef ref (msg :)
+import Control.Lens
 
 data FlushMsg = Flush
                 deriving (Exception, Show)
@@ -47,42 +20,80 @@ data FizzBuzzMsg = Fizz
                  | Buzz
                  | Num Int
                  deriving (Exception, Show)
+makePrisms ''FizzBuzzMsg
 
-mainLoop :: Int -> Int -> IO ()
-mainLoop untill cur = pure ()
+main :: IO ()
+main = do
+  putStrLn "startin"
+  emitter <- forkIO $ emitter
+  putStrLn "emmiter"
+  fizzTid <- forkIO $ fizz emitter
+  putStrLn "buzz"
+  buzzTid <- forkIO $ buzz emitter
+  putStrLn "numsender"
+  numsTid  <- forkIO $ numSender emitter
+  let threads = [fizzTid, buzzTid, numsTid]
+  forever $ do
+    void $ traverse (flip throwTo Flush) threads
+    threadDelay 0_0010_000 -- wait for exceptions to arrive from work threads to emitter
+    throwTo emitter Flush
+    threadDelay 0_0010_000 -- emitter needs some time to clear before dealing with new exceptions
+
+emitter :: IO ()
+emitter = mask $ \unmask -> do -- we use unmask to indicate when we're ready for new messages
+  ref <- newIORef []
+  forever $
+      handle (\case
+                   Flush -> do
+                     res <- readIORef ref
+                     writeIORef ref []
+                     if (hasn't (traversed . failing _Fizz _Buzz) res) then do
+                         void $ traverse print $ reverse $ res ^.. traversed . _Num
+                     else do -- has a fizz or buzz (or both
+                        traverseOf (traversed . _Fizz) (const $ putStr "Fizz") res
+                        traverseOf (traversed . _Buzz) (const $ putStr "Buzz") res
+                        putStr "\n"
+            ) $ handle (putItIn ref) $ unmask waitMsg
+
+putItIn :: IORef [FizzBuzzMsg] -> FizzBuzzMsg -> IO ()
+putItIn ref msg = modifyIORef ref (msg :)
 
 numSender :: ThreadId -> IO ()
-numSender tid = do
-    ref <- newIORef 0
+numSender tid = mask $ \unmask -> do
+    ref <- newIORef 1
     forever $
       handle (\case
                    Flush -> do
                      cur <- atomicModifyIORef ref (\num -> (num + 1, num))
                      throwTo tid $ Num cur
                      pure ()
-               ) waitMsg
+               ) $ unmask waitMsg
 
+-- this is a safe point, eg can receive exceptions
 waitMsg :: IO ()
 waitMsg = threadDelay 10_000_000
 
 fizz :: ThreadId -> IO ()
-fizz tid = forever $
-  handle (\case
+fizz tid = mask $ \unmask -> do
+  forever $ handle (\case
+              -- completely declerative I suppose,
+              -- we just catch a flush 3 times before sending fizz.
+              -- no slow modulo for us!
              Flush -> throwTo tid Fizz) $
     handle (\case
-             Flush -> waitMsg) $
+             Flush -> unmask waitMsg) $
       handle (\case
-              Flush -> waitMsg) $ waitMsg
+              Flush -> unmask waitMsg) $ unmask waitMsg
 
 buzz :: ThreadId -> IO ()
-buzz tid = do
-  handle (\case
+buzz tid = mask $ \unmask -> do
+  forever $ handle (\case
              Flush -> throwTo tid Buzz) $
     handle (\case
-             Flush -> waitMsg) $
+             Flush -> unmask waitMsg) $
     handle (\case
-             Flush -> waitMsg) $
+             Flush -> unmask waitMsg) $
     handle (\case
-             Flush -> waitMsg) $
+             Flush -> unmask waitMsg) $
       handle (\case
-              Flush -> waitMsg) $ waitMsg
+              Flush -> unmask waitMsg) $ unmask waitMsg
